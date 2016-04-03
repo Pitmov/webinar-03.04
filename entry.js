a8c0ee93adaa73675b7039d84048cf018e73de6a
Sergey Lapin <lapanoid@gmail.com>
2016-04-03 17:51:07 +0300

    initial commit

---
+++ entry.js
@@ -0,0 +1,6 @@
+require("./style.css");
+document.write("It works.");
+document.write("It worksasasass.");
+document.write("It works.");
+document.write(require("./content.js"));
+document.write(require("./content.js"));
